a6164c2f6f5875507e4fea68e0cd9fc086d358c2
ridewankasye6-lab <ridewankasye6@gmail.com>
2026-07-31 13:53:46 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -23,60 +23,70 @@ function showQuestion() {
     document.getElementById("question").innerHTML = q.question;
 
     document.getElementById("btn0").innerHTML = q.options[0];
-
     document.getElementById("btn1").innerHTML = q.options[1];
-
     document.getElementById("btn2").innerHTML = q.options[2];
-
     document.getElementById("btn3").innerHTML = q.options[3];
 
-}
+    document.getElementById("btn0").disabled = false;
+    document.getElementById("btn1").disabled = false;
+    document.getElementById("btn2").disabled = false;
+    document.getElementById("btn3").disabled = false;
 
-showQuestion();
+    document.getElementById("btn0").style.background = "";
+    document.getElementById("btn1").style.background = "";
+    document.getElementById("btn2").style.background = "";
+    document.getElementById("btn3").style.background = "";
 
-function checkAnswer(index){
-
-let q = questions[currentQuestion];
-
-if(index == q.correct){
-
-score++;
-
-document.getElementById("btn"+index).style.background = "green";
-
-}else{
-
-document.getElementById("btn"+index).style.background = "red";
-
-document.getElementById("btn"+q.correct).style.background = "green";
+    document.getElementById("nextBtn").style.display = "none";
 
 }
 
-document.getElementById("nextBtn").style.display = "block";
+function checkAnswer(index) {
+
+    let q = questions[currentQuestion];
+
+    document.getElementById("btn0").disabled = true;
+    document.getElementById("btn1").disabled = true;
+    document.getElementById("btn2").disabled = true;
+    document.getElementById("btn3").disabled = true;
+
+    if (index === q.correct) {
+
+        score++;
+
+        document.getElementById("btn" + index).style.background = "green";
+
+    } else {
+
+        document.getElementById("btn" + index).style.background = "red";
+
+        document.getElementById("btn" + q.correct).style.background = "green";
+
     }
-function nextQuestion(){
 
-currentQuestion++;
+    document.getElementById("nextBtn").style.display = "block";
 
-if(currentQuestion < questions.length){
+}
 
-document.getElementById("btn0").style.background = "";
-document.getElementById("btn1").style.background = "";
-document.getElementById("btn2").style.background = "";
-document.getElementById("btn3").style.background = "";
+function nextQuestion() {
 
-document.getElementById("nextBtn").style.display = "none";
+    currentQuestion++;
+
+    if (currentQuestion < questions.length) {
+
+        showQuestion();
+
+    } else {
+
+        localStorage.setItem("score", score);
+        localStorage.setItem("total", questions.length);
+        localStorage.setItem("subject", subject);
+        localStorage.setItem("chapter", chapter);
+
+        window.location.href = "result.html";
+
+    }
+
+}
 
 showQuestion();
-
-}else{
-
-localStorage.setItem("score", score);
-
-localStorage.setItem("total", questions.length);
-
-window.location.href = "result.html";
-
-}
-
-}
