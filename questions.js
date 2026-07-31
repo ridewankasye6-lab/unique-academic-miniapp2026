8041a9957c7dcb007907e557b971659a45aa1967
ridewankasye6-lab <ridewankasye6@gmail.com>
2026-07-31 08:41:47 +0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,3 +1,45 @@
 const QUESTIONS = {
 
+"global-trends":{
+
+1:[
+
+{
+question:"What does International Relations mainly study?",
+
+answers:[
+"Domestic politics",
+"Relations among states",
+"Agriculture",
+"Medicine"
+],
+
+correct:1,
+
+english:"International Relations studies relations among states and international actors.",
+
+amharic:"ዓለም አቀፍ ግንኙነት በመንግስታትና በሌሎች ዓለም አቀፍ ተዋናዮች መካከል ያለውን ግንኙነት ያጠናል።"
+},
+
+{
+question:"Which organization promotes international peace?",
+
+answers:[
+"NBA",
+"United Nations",
+"FIFA",
+"NASA"
+],
+
+correct:1,
+
+english:"The United Nations works to maintain international peace and security.",
+
+amharic:"የተባበሩት መንግስታት ዓለም አቀፍ ሰላምና ደህንነትን ለማስጠበቅ ይሰራል።"
+}
+
+]
+
+}
+
 };
