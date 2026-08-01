4007123271e6cccbdff8f10820be990d30bf0c9f
ridewankasye6-lab <ridewankasye6@gmail.com>
2026-08-01 19:20:57 +0300

    Update and rename questions.js to quizData.js

---
+++ quizData.js
@@ -0,0 +1,30 @@
+const quizData = {
+
+  "global-trends": {
+
+    1: [
+
+      {
+        question: "What is International Relations primarily concerned with?",
+
+        options: [
+          "Domestic Politics",
+          "Relations Between States",
+          "Economics Only",
+          "History Only"
+        ],
+
+        answer: 1,
+
+        englishExplanation:
+          "International Relations studies relationships between states, international organizations and global actors.",
+
+        amharicExplanation:
+          "የአለም አቀፍ ግንኙነት በመንግስታት፣ በዓለም አቀፍ ድርጅቶች እና በሌሎች ዓለም አቀፍ ተዋናዮች መካከል ያለውን ግንኙነት ያጠናል።"
+      }
+
+    ]
+
+  }
+
+};
