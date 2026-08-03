e2077dd861102f6e89bc18d304cc4f1f797d3fda
ridewankasye6-lab <ridewankasye6@gmail.com>
2026-08-03 12:05:11 +0300

    Update quizData.js

--- quizData.js
+++ quizData.js
@@ -2,7 +2,113 @@ const quizData = {
 
     "global-trends": {
 
-        1: [],
+        1: [[
+  {
+    "question": "1. IR is more concentrated on domestic relations.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 1,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 IR ሙሉ ትኩረቱ ሀገር ውስጥ ስላለ ግንኙነት (domestic relationship) ሳይሆን ድንበር ተሻጋሪ (Across boarder) በሆነ ግንኙነት ነው::"
+  },
+  {
+    "question": "2. Comparatively, multipolar world orders are most susceptible to instability and war.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 0,
+    "englishExplanation": "📌 The most stable የሚባለው Unipolar ነው",
+    "amharicExplanation": "📚 multipolar world በ IR የተለያዩ እኩል ስልጣን ያላቸው ኃይሎች መፈጠር ነው :: ብዙ ኃይል መፈጠር ደግሞ የ ሀሳብ አለመስማማት ላይ ይደርሳል ይህም ወደ ብጥብጥ ይሄዳል ::ለምሳሌ የመጀመሪያው የዓለም ጦርነትን ማየት ይቻላል ::"
+  },
+  {
+    "question": "3. As IR is contacted with many Global issues and its scope is wide, one can conclude that it is Holistic.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 1,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 IR ምንም እንኳ የሚያነሷቸው ሀሳቦች ሰፊ ቢሆኑም Holistic( ሁሉን ዓቀፍ) ነው ማለት አንችልም:: ምክንያቱም ድንበር ያልተለለ የሀገር ውስጥ ግንኙነቶችን አያጠቃልልም ::የ Holistic ፅንሰ ሀሳብ ደግሞ global ንም local ንም ስለሚጨምር::"
+  },
+  {
+    "question": "4. Terrorists can be actor of IR.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 0,
+    "englishExplanation": "📚 Terrorists can be considered actors in International Relations (IR). While they may not be traditional state actors like governments or international organizations, terrorist groups influence global politics, security, and conflict dynamics. They can challenge state sovereignty, provoke military interventions, and affect diplomacy.",
+    "amharicExplanation": "📚 አሸባሪዎች በዓለም አቀፍ ግንኙነቶች አስተዋፅኦ እስከ አደረጉ ድረስ Actors ሊሆኑ ይችላሉ ::"
+  },
+  {
+    "question": "5. For Marxists, the international system is characterized by anarchy and class contradiction.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 1,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 ለ Marxists ዓለም አቀፍ ስርዓቱ(international system) በ ሁለት ቡድኖች/Class የተከፈለ ነው ያም ማለት በ ባለሀብቶች ና በ ድሆች::\n\n📚 Marxists ርዕዮተ ዓለም የምያራምዱት ሀሳብ ድሆች በ ባለጸጎች ስለተበደሉ እኩልነት እንዲኖር ና Class Contradiction ወይም መከፋፈል እንዳይኖር ማድረግ ላይ ነው::"
+  },
+  {
+    "question": "6. For realist thinkers of international relations, states' behavior is driven by moral and value.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 1,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 የ Realist አቀንቃኞች የ ሀገራት ባህርያት መሠረት ወይም ሀገራት ነገሮችን የሚያከናውኑበት ምክንያት(ለምሳሌ ለድሀ ሀገራት እርዳታ ቢሰጡ) ሞራላዊ የሆነ ስሜት ስላላቸው ሳይሆን ለስልጣን እና ለ ራሳቸው ጥቅም ብለው እንደሆነ ነው የሚያስቡት"
+  },
+  {
+    "question": "7. Realists hold the position that the state, in its relation with another state, is definitely subject to the calculations of morality.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 1,
+    "englishExplanation": "📚 በተጨማሪም realists argue that the need for survival requires state leaders to distance themselves from traditional notions of morality.",
+    "amharicExplanation": "📚 በተደጋጋሚ ስለሚመጣ ያዙት:: ከላይ እንዳልክላችሁ በ Realist ሀገራት ከሌላ ሀገራት ጋር ግንኙነት የሚያፈጥሩት ለ National interest(ለ ሀገራቸው/ለራሳቸው ጥቅም) እንደሆነ ይታሰባል ::"
+  },
+  {
+    "question": "8. Conventional IR was not concerned with any potential distinctions between different levels of analysis or theoretical perspectives.",
+    "options": [
+      "True",
+      "False"
+    ],
+    "answer": 0,
+    "englishExplanation": "Conventional International Relations (IR), in its traditional form, was not concerned with potential distinctions between different levels of analysis or theoretical perspectives. Traditional IR focused more on state-centric approaches and the interactions",
+    "amharicExplanation": "💡 Hint: Conventional ማለት traditional ማለት ነው::"
+  },
+  {
+    "question": "19. Which international relation theories can be taken as utopian?",
+    "options": [
+      "Liberalism",
+      "Marxism",
+      "Idealism",
+      "A and C",  
+    ],
+    "answer": 3,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 utopian ሀሳባዊ(ideal) የሆነ ፅንሰ ሀሳብ ሲሆን የሚያመላክተውም by nature perfect፤ደግ እትሁት የሆነ ማህበረሰብ ወይም አለምን ነው::"
+  },
+  {
+    "question": "20. ____________ can be defined as a state's ultimate authority within its territorial entity?",
+    "options": [
+      "Internal sovereignty",
+      "External sovereignty",
+      "Authority",
+      "Anarchy"
+    ],
+    "answer": 0,
+    "englishExplanation": "",
+    "amharicExplanation": "📚 internal sovereignty አንዲት ሀገራ በራሷ ግዛት(በራሷ ደንበር ውስጥ) የፈለገችው ነገር ያለምንም የውጭ ሀገራት ጣልቃ ገብነት የማድረግ ሙሉ ሀይል እንዳላት ይገልጣል :: ለምሳሌ ግብር መሰብሰብ፤የ ሀገሪቱን የጦር ሀይል ማደራጀት የመሳሰሉት ....በሀገሪቱ ውስጥ::"
+  }
+]
+ ],
         2: [],
         3: [],
         4: [],
